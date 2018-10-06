4ff523f04070a51820197cb9c30cd944ceab663c
AliAdelAli94 <aliadel721@gmail.com>
2018-10-06 22:58:41 +0200

    First Commit all Solution

---
+++ LawFirm.Controls/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LawFirm.Controls.Global" Language="C#" %>
---
+++ LawFirm.Controls/app/js/angularjs/app.js
@@ -0,0 +1,87 @@
+﻿(function (ng) {
+    'use strict';
+
+    var app = ng.module("LawFirmApp", ['ngRoute']);
+
+    app.config(function ($routeProvider,$locationProvider) {
+        $routeProvider
+            .when("/index.html", {
+                templateUrl: "home.html"
+            })
+            .when("/", {
+                templateUrl: "home.html"
+            })
+            .when("/about-us.html", {
+                templateUrl: "about-us.html"
+            })
+            .when("/green", {
+                templateUrl: "green.htm"
+            })
+            .when("/blue", {
+                templateUrl: "blue.htm"
+            });
+
+        $locationProvider.html5Mode(true);
+    });
+
+    app.controller("about-us",function($route){
+
+        load_js();
+    });
+
+
+    app.controller("home",function($route){
+
+        load_js();
+    });
+
+}(angular));
+
+function load_js()
+{
+    var head= document.getElementsByTagName('head')[0];
+    var script= document.createElement('script');
+    script.src= 'js/script.js';
+    head.appendChild(script);
+}
+
+/*
+
+
+var app = angular.module("LawFirmApp", ['ngRoute','ngLoadScript']);
+
+app.config(function ($routeProvider,$locationProvider) {
+    $routeProvider
+    .when("/index.html", {
+        templateUrl: "home.html"
+    })
+    .when("/", {
+        templateUrl: "home.html"
+    })
+    .when("/about-us.html", {
+        templateUrl: "about-us.html"
+    })
+    .when("/green", {
+        templateUrl: "green.htm"
+    })
+    .when("/blue", {
+        templateUrl: "blue.htm"
+    });
+
+    $locationProvider.html5Mode(true);
+
+});
+
+app.directive('script', function() {
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function (scope, elem, attr) {
+            if (attr.type == 'text/javascript-lazy') {
+                var code = elem.text();
+                var f = new Function(code);
+                f();
+            }
+        }
+    }
+});*/
